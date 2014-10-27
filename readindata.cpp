42a7be3fb3a1ed54cbb18ba865fbfc1b88d6f55c
Chun Shen <chunshen1987@gmail.com>
2014-10-26 22:43:09 -0400

    fixed a typo

--- readindata.cpp
+++ readindata.cpp
@@ -139,7 +139,7 @@ void read_FOdata::read_decdat(int length, FO_surf* surf_ptr)
 
      decdat >> temp_vx;
      decdat >> temp_vy;
-     surf_ptr[i].u0 = sqrt(1. + temp_vx*temp_vx + temp_vy*temp_vy);
+     surf_ptr[i].u0 = 1./sqrt(1. - temp_vx*temp_vx - temp_vy*temp_vy);
      surf_ptr[i].u1 = surf_ptr[i].u0*temp_vx;
      surf_ptr[i].u2 = surf_ptr[i].u0*temp_vy;
      surf_ptr[i].u3 = 0.0;
