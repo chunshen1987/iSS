2556f863d3f082ac1bdcfa269d981434eb34868d
Chun Shen <chunshen1987@gmail.com>
2016-02-12 21:19:27 -0500

    fixed sig fault

--- src/main.h
+++ src/main.h
@@ -6,7 +6,7 @@ using namespace std;
 const double hbarC=0.197327053;  //GeV*fm
 
 const int Maxparticle=400;            //size of array for storage of the particles
-const int Maxdecaychannel=13;
+const int Maxdecaychannel=16;
 const int Maxdecaypart=5;
 
 #endif
