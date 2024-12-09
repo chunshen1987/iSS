3f436799b4555a530f29e3eb7a55c02f7378d3cd
Chun Shen <chunshen@wayne.edu>
2024-12-09 11:57:28 -0500

    add a download script for the new RTA delta f coeffs

---
+++ iSS_tables/deltaf_tables/download_newRTA.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget --no-check-certificate 'https://www.dropbox.com/scl/fi/guie8afr2uc1jdmppjsrk/newRTA.tar.gz?rlkey=z8urab09pv50ty58tqs3dip7g&dl=0' -O newRTA.tar.gz
+tar -zxvf newRTA.tar.gz
+rm newRTA.tar.gz
